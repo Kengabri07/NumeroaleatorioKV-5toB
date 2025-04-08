8e4707bbb76c408de46a0c2855e4017ab6610269
JonathanPaz-coder <paz.jonathan.loyola@gmail.com>
2025-04-08 14:13:54 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace NumeroaleatorioKV_5toB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtintentos = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Titulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Intentos";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // txtintentos
+            // 
+            this.txtintentos.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtintentos.Location = new System.Drawing.Point(176, 75);
+            this.txtintentos.Name = "txtintentos";
+            this.txtintentos.Size = new System.Drawing.Size(156, 32);
+            this.txtintentos.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 26);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Adivina ";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(176, 142);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(156, 32);
+            this.txtNumero.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 89);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Titulo
+            // 
+            this.Titulo.AutoSize = true;
+            this.Titulo.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titulo.Location = new System.Drawing.Point(41, 25);
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Size = new System.Drawing.Size(253, 31);
+            this.Titulo.TabIndex = 5;
+            this.Titulo.Text = "¡Adivina el numero!";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(344, 339);
+            this.Controls.Add(this.Titulo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtintentos);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtintentos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Titulo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumeroaleatorioKV_5toB
+{
+    public partial class Form1 : Form
+    {
+        int intentos = 0; //Inicializar la Variable
+        Random rand = new Random(); //Crea eñ objeto que genere el número aleatorio
+        int aleatorio = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            intentos = 5;
+            txtintentos.Text = intentos.ToString();
+            aleatorio = rand.Next(1, 100); // aleatorio = número entre 1 y 100
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtNumero.Text, out int num))
+            {
+                if (num == aleatorio)
+                {
+                    MessageBox.Show($"Gano el juego. El numero es {aleatorio}");
+                    DialogResult resultado = MessageBox.Show("¿Quiere volver a jugar?", "Resultado", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (resultado == DialogResult.No)
+                    {
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        intentos = 5;
+                        txtintentos.Text = intentos.ToString();
+                        aleatorio = rand.Next(1, 100); // aleatorio = número entre 1 y 100cdsfsvfngbbd f ytjf  67jyr f i56tfb54 6tref rg3 4redg 
+
+                    }
+                }
+
+                if (num > aleatorio)
+                {
+                    MessageBox.Show($"{num} es mayor al numero aleatorio");
+                }
+
+                if (num < aleatorio)
+                {
+                    MessageBox.Show($"{num} es menor al numero aleatorio");
+                }
+
+            }
+
+        }
+    }   
+}
